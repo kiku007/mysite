846164f30fc81da379fc7e0cf0458897b2f9a1d2
kiku <tkikugawa7@gmail.com>
2020-02-16 14:15:45 +0900

    Complete home.php

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+  <div class="main">
+    <div class="content">
+      <h1><?php bloginfo('name'); ?><p>- kiku -</p></h1>
+      <p class="since">Since 2020</p>
+      <p class="message">Webサイト制作を行っております。お問い合わせはご案内ページ「about」からメールでどうぞ。
+         またTwitterでもお受けします。
+      </p>
+    </div>      
+<?php get_sidebar(); ?>
+  </div>
+<?php get_footer(); ?>
